1d368c27dcca813a363dc04ae9b3349ddf95fb2f
DKFrederik <dkfrede@gmail.com>
2017-03-30 09:42:03 +0200

    Added another CPU thread Added a start button Added a stop button added a close application button

--- GraphClient/Form1.cs
+++ GraphClient/Form1.cs
@@ -22,9 +22,11 @@ namespace GraphClient
 
 
         private Thread cpuThread;
-        //This is the time shown on the diagram
-        private double[] cpuArray = new double[60];
-        
+        private Thread cpuThread1;
+        //Inside [ ] is the time shown on the diagram
+        private double[] cpuArray = new double[120];
+        private double[] cpuArray1 = new double[120];
+
 
         public Form1()
         {
@@ -37,7 +39,10 @@ namespace GraphClient
             while (true)
             {
                 cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerformanceCounter.NextValue(), 0);
+                cpuArray1[cpuArray.Length - 1] = Math.Round(cpuPerformanceCounter.NextValue(), 0);
                 Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+                Array.Copy(cpuArray1, 1, cpuArray1, 0, cpuArray1.Length - 1);
+
 
                 if (chart.IsHandleCreated)
                 {
@@ -45,36 +50,46 @@ namespace GraphClient
                 }
                 else
                 {
+                    break;
                     //Do somthing 
                 }
 
-                //This is the update interval
+                //Inside ( ) is the update interval
                 Thread.Sleep(400);
             }
         }
 
         private void UpdateCpuChart()
         {
-            chart.Series["Series1"].Points.Clear();
+            chart.Series["CPU1"].Points.Clear();
+            chart.Series["CPU2"].Points.Clear();
 
             for (int i = 0; i < cpuArray.Length - 1; ++i)
             {
-                chart.Series["Series1"].Points.AddY(cpuArray[i]);
+                chart.Series["CPU1"].Points.AddY(cpuArray[i]);
+                chart.Series["CPU2"].Points.AddY(cpuArray1[i]);
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            cpuThread = new Thread(new ThreadStart(this.GetPrefomanceCounters));
-            cpuThread.IsBackground = true;
-            cpuThread.Start();
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
             cpuThread = new Thread(new ThreadStart(this.GetPrefomanceCounters));
+            cpuThread1 = new Thread(new ThreadStart(this.GetPrefomanceCounters));
+            cpuThread.IsBackground = true;
             cpuThread.IsBackground = true;
             cpuThread.Start();
+            cpuThread1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cpuThread.Abort();
+            cpuThread1.Abort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
